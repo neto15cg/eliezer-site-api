9582b534f296a1426fd8ccf54bad74d32a35ca58
Eliezer Marques <neto15.cg@gmail.com>
2025-02-23 16:41:08 -0300

    feat: add response field to Message model

--- models/message.go
+++ models/message.go
@@ -14,6 +14,7 @@ type Validator interface {
 type Message struct {
 	ID             uuid.UUID  `json:"id" gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
 	Message        string     `json:"message" binding:"required"`
+	Response       *string    `json:"response"`
 	ConversationID *uuid.UUID `json:"conversation_id" binding:"required"`
 	CreatedAt      time.Time  `json:"created_at"`
 	UpdatedAt      time.Time  `json:"updated_at"`
